aecac471f7df359ebbf881c57b5c2ed1f93087b0
SurajNyavanandi <surajdec11@gmail.com>
2026-03-11 16:49:40 +0530

    Revised JSX,Class & Functional Components

---
+++ src/StudentComponent.js
@@ -0,0 +1,12 @@
+const Student =()=>{
+    return (
+    <>
+      <h4>Student Component</h4>
+      <h3>Name: Jai Shree Rama</h3>
+      <h3>Age: 26</h3>
+      <h3>Friend:Om Namah Shivaya</h3>          
+    </>
+    )
+};
+
+export default Student;
